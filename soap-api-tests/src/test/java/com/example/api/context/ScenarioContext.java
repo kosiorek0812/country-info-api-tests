887736c8516168cdce13e7df667fde23df424aa1
kosiorek0812 <pawel.m.kosiorek@gmail.com>
2026-06-04 17:05:20 +0200

    classes

--- soap-api-tests/src/test/java/com/example/api/context/ScenarioContext.java
+++ soap-api-tests/src/test/java/com/example/api/context/ScenarioContext.java
@@ -2,11 +2,14 @@ package com.example.api.context;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
 @Setter
+@Scope("cucumber-glue")
 public class ScenarioContext {
 
     private String responseXml;
