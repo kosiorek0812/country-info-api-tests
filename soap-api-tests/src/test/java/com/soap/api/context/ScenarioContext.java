e220489025436d452c4b8d5281a35a9bbf1449fd
kosiorek0812 <pawel.m.kosiorek@gmail.com>
2026-06-05 14:00:11 +0200

    delete target dir

--- soap-api-tests/src/test/java/com/soap/api/context/ScenarioContext.java
+++ soap-api-tests/src/test/java/com/soap/api/context/ScenarioContext.java
@@ -1,7 +1,8 @@
-package com.example.api.context;
+package com.soap.api.context;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 @Scope("cucumber-glue")
 public class ScenarioContext {
 
-  private String responseXml;
-  private int statusCode;
+    private String responseXml;
+    private int statusCode;   
+    
 }
