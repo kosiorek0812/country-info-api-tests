dd1e808151ae2c7e854b30df83330e3418601850
kosiorek0812 <pawel.m.kosiorek@gmail.com>
2026-06-05 08:35:22 +0200

    renamed

--- soap-api-tests/src/test/java/com/example/api/runner/TestRunner.java
+++ soap-api-tests/src/test/java/com/example/api/runner/TestRunner.java
@@ -1,12 +1,15 @@
 package com.example.api.runner;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.ConfigurationParameter;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
 public class TestRunner {
-    
 }
